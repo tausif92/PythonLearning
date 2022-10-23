72643151293c8b7f2da5a7c8f9fbc9e52bec92d4
Tausif Bajaria <mtbajaria257@gmail.com>
2022-10-23 23:13:04 +0530

    String basics

---
+++ Basics/string.py
@@ -0,0 +1,48 @@
+
+str_1 = "hello woRld!"
+print("Original string: " + str_1)
+# String indexing is used to grab a character at a particular index
+first_char = str_1[0]
+last_char = str_1[-1]
+
+# String slicing is used to grab the multiple characters by giving the starting and finishing index
+# str_1[starting_index, finishing_index, step_size]
+slice_1 = str_1[2:]   # llo World!
+slice_2 = str_1[2:7]  # llo W
+slice_3 = str_1[:7]   # Hello W
+slice_4 = str_1[::2]  # HloWrd
+
+# String concatenation
+concat = "This is an example of concatenation:" + slice_1 + slice_2
+
+# Count number of occurrences of letter "l":
+print("Number of occurrences of l is: " + str(str_1.count("l")))
+
+# Find index of the first occurrence of a character. Returns -1 if the char is not found.
+print("Index of w (first occurrence) in the string is: " + str(str_1.find("w")))
+
+# Capitalize string
+print("Capitalized string is: " + str_1.capitalize())
+
+# Returns boolean True if the string is in lower case else returns False
+print("Is string in lower case: " + str(str_1.islower()))
+
+print("Is string in upper case: " + str(str_1.isupper()))
+
+print("String in upper case: " + str(str_1.upper()))
+
+print("String in lower case: " + str_1.lower())
+
+print("Replace o with O: " + str_1.replace("o", "O"))
+
+print("Splits by white spaces: " + str(str_1.split()))
+
+print("Splits by character l: " + str(str_1.split("l")))
+
+# Format string
+print("The {} can {}.".format("dog", "bark"))
+
+print("Round off 3 decimal places: {decimal_number:1.3f}".format(decimal_number=104.12345))
+
+# New formatting syntax
+print(f"The string is: {str_1}")
